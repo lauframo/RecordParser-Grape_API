7d923284a2190b277d2338a8bea64ef8e9339e4e
Laura Moreno <stereofonik.imgs@gmail.com>
2017-01-25 16:27:10 -0600

    define parsing logic to accept pipe and comma delimitors

--- record_parser.rb
+++ record_parser.rb
@@ -1,2 +1,28 @@
+require 'csv'
+require_relative 'person'
 module RecordParser
+
+  def self.parse(filename)
+    output = []
+    CSV.open(filename, 'r') do |csv|
+      if csv.first[0].include? "|"
+        CSV.foreach(filename, :headers => true, :col_sep => "|") do |row|
+          new_row = (row.to_hash)
+          output << Person.new(new_row)
+        end
+      else
+        p "got here"
+         CSV.foreach(filename, :headers => true) do |row|
+          new_row = (row.to_hash)
+          output << Person.new(new_row)
+        end
+      end
+    end
+    output
+  end
+
 end
+
+
+# p RecordParser.parse("records.csv")
+# p RecordParser.parse("records_pipe_delimited.csv")
