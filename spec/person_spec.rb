f54c3bd527f474876431aa8dd539c6b433515e2c
Laura Moreno <stereofonik.imgs@gmail.com>
2017-01-25 19:02:17 -0600

    additional tests to ensure all instances are returnes. all tests failed

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -5,10 +5,10 @@ require_relative '../person'
 describe Person do
   let(:laura_args) { {"FirstName"=> "Laura", "LastName"=> "Moreno", "FavoriteColor"=> "cerulean", "DateOfBirth"=> '2017-01-23' } }
   let(:laura) { Person.new(laura_args)}
-  # let(:josh_args) { { "FirstName"=> "Josh", "LastName"=> "Lyman", "FavoriteColor"=> "tan", "DateOfBirth"=> '2017-01-21' } }
-  # let(:josh) { Person.new(josh_args)}
-  # let(:donna_args) { { "FirstName"=> "Josh", "LastName"=> "Lyman", "FavoriteColor"=> "tan", "DateOfBirth"=> '2017-01-21' } }
-  # let(:donna) { Person.new(donna_args)}
+  let(:josh_args) { { "FirstName"=> "Josh", "LastName"=> "Lyman", "FavoriteColor"=> "tan", "DateOfBirth"=> '2017-01-19' } }
+  let(:josh) { Person.new(josh_args)}
+  let(:donna_args) { { "FirstName"=> "Donna", "LastName"=> "Moss", "FavoriteColor"=> "amber", "DateOfBirth"=> '2016-02-25' } }
+  let(:donna) { Person.new(donna_args)}
 
   context 'it has readeable attributes' do
 
@@ -29,15 +29,26 @@ describe Person do
     end
   end
 
+  context 'it returns all instances of the Person class' do
+
+  	 it 'returns laura, josh and donna in array' do
+  	 	expect(Person.all).to include(laura, donna, josh)
+  	 end
+
+  end
+
   context 'it sorts by desired attribute' do
 
     it 'sorts by favorite color descending, then by last name ascending' do
+    	expect(Person.sort_by_color).to eq([josh, laura, donna])
     end
 
     it 'sorts by birth date ascending' do
+    	expect(Person.sort_by_date).to eq([donna, josh, laura])
     end
 
     it 'sorts by last name descending' do
+    	expect(Person.sort_by_surname).to eq([donna, laura, josh])
     end
 
   end
