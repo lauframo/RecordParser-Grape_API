99546a6d74b788e5fc7facc7fac08219d4dae01e
Laura Moreno <stereofonik.imgs@gmail.com>
2017-01-28 11:25:08 -0600

    update hash_output method as a class method. not satisfied with implementation ...

--- app/models/person.rb
+++ app/models/person.rb
@@ -24,12 +24,9 @@ class Person
   end
 
   def self.sort_by_color
-    array = []
-    collection = self.all.sort do |a, b|
+    self.all.sort do |a, b|
       [b.favorite_color.downcase, a.last_name.downcase] <=> [a.favorite_color.downcase, b.favorite_color.downcase]
     end
-    collection.each { |instance| array << instance.to_hash }
-    array
   end
 
   def self.sort_by_birthdate
@@ -44,6 +41,12 @@ class Person
     end
   end
 
+  def self.hash_ouput(collection)
+    array = []
+    collection.each { |instance| array << instance.to_hash }
+    array
+  end
+
 
   def save(database_object)
     database_object.add(self)
@@ -52,28 +55,28 @@ class Person
 end
 
 
-laura_args = {"FirstName"=> "Laura", "LastName"=> "Moreno", "FavoriteColor"=> "cerulean", "DateOfBirth"=> '2017-01-23' }
-laura = Person.new(laura_args)
-# # p laura.to_hash
-# # hash = {}
-# # laura.instance_variables.each {|var| hash[var.to_s.delete("@")] = laura.instance_variable_get(var) }
-# # p hash
-# # p hash # => {"name"=>"book", "price"=>15.95}
-# # test = {:first_name => laura.first_name, :last_name=>laura.last_name}
+# laura_args = {"FirstName"=> "Laura", "LastName"=> "Moreno", "FavoriteColor"=> "cerulean", "DateOfBirth"=> '2017-01-23' }
+# laura = Person.new(laura_args)
+# # # p laura.to_hash
+# # # hash = {}
+# # # laura.instance_variables.each {|var| hash[var.to_s.delete("@")] = laura.instance_variable_get(var) }
+# # # p hash
+# # # p hash # => {"name"=>"book", "price"=>15.95}
+# # # test = {:first_name => laura.first_name, :last_name=>laura.last_name}
 
-# # p test
-josh_args = { "FirstName"=> "Josh", "LastName"=> "Lyman", "FavoriteColor"=> "tan", "DateOfBirth"=> '2017-01-19' }
-josh = Person.new(josh_args)
-# p josh.to_json
-# # p josh.to_hash
+# # # p test
+# josh_args = { "FirstName"=> "Josh", "LastName"=> "Lyman", "FavoriteColor"=> "tan", "DateOfBirth"=> '2017-01-19' }
+# josh = Person.new(josh_args)
+# # p josh.to_json
+# # # p josh.to_hash
 # donna_args = { "FirstName"=> "Donna", "LastName"=> "Moss", "FavoriteColor"=> "amber", "DateOfBirth"=> '2016-02-25' }
 # donna =  Person.new(donna_args)
-# # # # database = Database.new
+# # # # # database = Database.new
 
-# people = []
-# Person.sort_by_surname.each {|person| people << person.to_hash }
-# p people
-p Person.sort_by_color
+# # people = []
+# # Person.sort_by_surname.each {|person| people << person.to_hash }
+# # p people
+# p Person.hash_ouput(Person.sort_by_birthdate)
 # # # p Person.sort_by_birthdate
 # # # p Person.sort_by_surname
 
