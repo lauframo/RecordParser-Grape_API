2eaa68408b609547c15e4ae859c89d9d8f9762ee
Laura Moreno <stereofonik.imgs@gmail.com>
2017-01-29 23:32:49 -0600

    add test for missing data

--- spec/api/api_spec.rb
+++ spec/api/api_spec.rb
@@ -9,9 +9,11 @@ describe API do
 	let!(:browser) { Rack::Test::Session.new(Rack::MockSession.new(app)) }
 
 	describe 'POST api/v1/records' do
-		record_params = { "LastName" => "Moreno", "FirstName" => "Laura", "FavoriteColor" => "blue", "DateOfBirth" => "2010-11-08"}
-		browser.post 'api/v1/records', record_params	
+
 		context 'with valid data' do
+		record_params = { "LastName" => "Moreno", "FirstName" => "Laura", "FavoriteColor" => "blue", "DateOfBirth" => "2010-11-08"}
+		browser.post 'api/v1/records', record_params
+
 			it 'returns a 200 status code' do
 				expect(browser.last_response.status).to eq(200)
 			end
@@ -22,8 +24,19 @@ describe API do
 				database = File.read('database_file.csv')
 				expect(database.last).to eq "Moreno,Laura,Blue,2010-11-08\n"
 			end
+		end
+	
+		context 'with invalid data' do 
+			record_params = { "FirstName" => "Laura", "FavoriteColor" => "blue", "DateOfBirth" => "2010-11-08"}
+			browser.post 'api/v1/records', record_params
 
+			it 'returns an error' do
+				response = (JSON.parse(last_response.body)
+				expect(response['message']).to eq("Last Name can't be blank")
+
+			end
 		end
 	end
+
 
 end
