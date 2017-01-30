f88d8b259014f8507bf3df2e9b8b447f5d38bd98
Laura Moreno <stereofonik.imgs@gmail.com>
2017-01-30 17:25:28 -0600

    declare required and optional params. play with post method

--- app/api/v1/api.rb
+++ app/api/v1/api.rb
@@ -1,21 +1,25 @@
 require 'grape'
 
-# module WestWing
-	class API < Grape::API
-	  version 'v1', using: :path
-	  format :json
+module WestWing
+  class API < Grape::API
+    version 'v1', using: :path
+    format :json
 
-   # p RecordParser.parse_file('records.csv')
-   #  @laura= Person.new({"FirstName"=> "Laura", "LastName"=> "Moreno", "FavoriteColor"=> "green", "DateOfBirth"=> '2015-10-21' })
-   #  @person = Person.new({"FirstName"=> "Jed", "LastName"=> "Barlett", "FavoriteColor"=> "navy", "DateOfBirth"=> '2015-10-20' })
+    @laura= Person.new({"FirstName"=> "Laura", "LastName"=> "Moreno", "FavoriteColor"=> "green", "DateOfBirth"=> '2015-10-21' })
+    @person = Person.new({"FirstName"=> "Jed", "LastName"=> "Barlett", "FavoriteColor"=> "navy", "DateOfBirth"=> '2015-10-20' })
+
+  params do
+    requires :LastName, type: String
+    requires :FirstName, type: String
+    requires :FavoriteColor, type: String
+    requires :DateOfBirth, type: String
+  end
 
 
-  # post do
-  #   person = Person.new(params)
-  #   person.save
-    
-
-  # end
+  post do
+    record = Person.new(params)
+    record.save(DATABASE)
+  end
 
     get :color do
       { records: Person.hash_ouput(Person.sort_by_color) }
@@ -29,4 +33,4 @@ require 'grape'
       { records: Person.hash_ouput(Person.sort_by_birthdate) }
     end
  	end
-# end
+end
