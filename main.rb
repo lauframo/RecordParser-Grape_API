989112cd0edb688689d99719c9700b2ef6d6409c
Laura Moreno <stereofonik.imgs@gmail.com>
2017-01-29 19:02:21 -0600

    declare global constant for database

--- main.rb
+++ main.rb
@@ -3,3 +3,5 @@ require './dependencies'
 (Dir['./app/models/*.rb'].sort + Dir['./app/api/*.rb'] + Dir['./app/helpers/*.rb'] + Dir['./app/routes/*.rb'].sort).each do |file|
   require file
 end
+
+DATABASE = Database.new('database.csv')
