cb7f7aaab28db87516e33aa8b512f6f76910a338
Laura Moreno <stereofonik.imgs@gmail.com>
2017-01-29 19:55:33 -0600

    update required files to go into controllers directory, delete routes

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require './dependencies'
 # (Dir['./app/models/*.rb'].sort + Dir['./app/api/*.rb'] + Dir['./app/routes/*.rb'].sort).each do |file|
-(Dir['./app/models/*.rb'].sort + Dir['./app/api/v1/*.rb'] + Dir['./app/helpers/*.rb'] + Dir['./app/routes/*.rb'].sort).each do |file|
+(Dir['./app/models/*.rb'].sort + Dir['./app/api/v1/*.rb'] + Dir['./app/helpers/*.rb'] + Dir['./app/controllers/*.rb'].sort).each do |file|
   require file
 end
 
